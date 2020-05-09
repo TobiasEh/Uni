b5b8eb2f22f947263f923289bd04bd34e4c82641
boehmeth <thomas.boehme@student.uni-augsburg.de>
2020-05-09 16:52:03 +0200

    Tb Blatt 3 Aufgabe 4

---
+++ Abgaben/Einzelabgaben/Böhme/Übungsblatt 3/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Blatt3_Aufgabe4.Models
+{
+	public class Booking
+	{
+		public int chargeStatus { get; set; }
+		public int distance { get; set; }
+		public DateTime startTime { get; set; }
+		public DateTime endTime { get; set; }
+
+		public Booking()
+		{
+		}
+	}
+}
