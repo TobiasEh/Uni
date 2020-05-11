bf4b843e978c7ef60e694a37e9192afb01976ef9
Tobias Eh <tobieh99@gmail.com>
2020-05-11 10:47:11 +0200

    Einzelabgabe Tobias Blatt 3

---
+++ Abgaben/Einzelabgaben/Eh/Übungsblatt3/Aufgabe 4/Controllers/BookingController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Aufgabe_4.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Aufgabe_4.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            Booking b1 = new Booking()
+            {
+                currentCharge = 10,
+                requiredDistance = 30,
+                start = new DateTime(2020, 5, 7, 12, 0, 0),
+                end = new DateTime(2020, 5, 7, 17, 0, 0)
+            };
+
+            Booking b2 = new Booking()
+            {
+                currentCharge = 10,
+                requiredDistance = 30,
+                start = new DateTime(2020, 5, 10, 12, 30, 0),
+                end = new DateTime(2020, 5, 10, 18, 0, 0)
+            };
+
+            Booking b3 = new Booking()
+            {
+                currentCharge = 10,
+                requiredDistance = 30,
+                start = new DateTime(2020, 5, 7, 22, 0, 0),
+                end = new DateTime(2020, 5, 8, 8, 15, 0)
+            };
+            Booking b4 = new Booking()
+            {
+                currentCharge = 10,
+                requiredDistance = 30,
+                start = new DateTime(2020, 5, 8, 22, 0, 0),
+                end = new DateTime(2020, 5, 9, 8, 15, 0)
+            };
+
+            List<Booking> bookingList = new List<Booking>()
+            {
+                b1,
+                b2,
+                b3,
+                b4
+            };
+            return View(bookingList);
+        }
+    }
+}
---
+++ Abgaben/Einzelabgaben/Eh/Übungsblatt3/Aufgabe 4/Models/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aufgabe_4.Models
+{
+    public class Booking
+    {
+        public double currentCharge { get; set; }
+        public int requiredDistance { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+    }
+}
