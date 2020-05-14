ca93cd91dd841841c40f54cd1565e38a7f56179f
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-05-14 02:42:11 +0200

    ü4a4
    vorläufig

--- Abgaben/Einzelabgaben/Nguyen/sopro_sose_2020/sopro_sose_2020/Models/Booking.cs
+++ Abgaben/Einzelabgaben/Nguyen/sopro_sose_2020/sopro_sose_2020/Models/Booking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,21 @@ namespace sopro_sose_2020.Models
 {
     public class Booking
     {
-        public double cur_charge { get; set; }
+        [Range(0, 100)]
+        [Required]
+        public int cur_charge { get; set; }
+
+        [Range(0, 1000)]
+        [Required]
         public int needed_distance { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
         public DateTime startTime { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
         public DateTime endTime { get; set; }
+
     }
 }
