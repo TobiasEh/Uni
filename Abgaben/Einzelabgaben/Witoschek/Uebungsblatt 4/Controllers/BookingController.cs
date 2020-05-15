b14dbdd563e405c942ab62d8c5f7271dc0695880
PimpMochi <62108424+PimpMochi@users.noreply.github.com>
2020-05-15 17:45:30 +0200

    B4A4

---
+++ Abgaben/Einzelabgaben/Witoschek/Uebungsblatt 4/Controllers/BookingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blatt03.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Blatt03.Controllers
+{
+    public class BookingController : Controller
+    {
+        List<Booking> bookings = new List<Booking>();
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Booking());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Booking booking)
+        {
+            bookings.Add(booking);
+            return View("Index", bookings);
+        }
+    }
+}
