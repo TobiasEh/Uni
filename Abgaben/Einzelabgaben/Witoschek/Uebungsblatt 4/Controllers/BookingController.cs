386366114f4d7577b3a458e9bab91794a0ccb02c
Dominik Witoschek <dominik.witoschek@student.uni-augsburg.de>
2020-05-16 16:14:16 +0200

    Revert "B4A4"
    This reverts commit 6851064fce23d6bccce3446ec07c41f2c68e79d3

--- Abgaben/Einzelabgaben/Witoschek/Uebungsblatt 4/Controllers/BookingController.cs
+++ Abgaben/Einzelabgaben/Witoschek/Uebungsblatt 4/Controllers/BookingController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Blatt03.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections;
-using Microsoft.Extensions.Caching.Memory;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,19 +12,9 @@ namespace Blatt03.Controllers
 {
     public class BookingController : Controller
     {
-        private readonly IMemoryCache _memoryCache;
         List<Booking> bookings = new List<Booking>();
-
-        public BookingController(IMemoryCache memoryCache)
-        {
-            _memoryCache = memoryCache;
-        }
-
         public IActionResult Index()
         {
-            var cacheKey = "bookings";
-            _memoryCache.TryGetValue(cacheKey, out bookings);
-
             return View(bookings);
         }
 
@@ -38,15 +27,7 @@ namespace Blatt03.Controllers
         [HttpPost]
         public IActionResult Post(Booking booking)
         {
-            var cacheKey = "bookings";
-
-            if (!_memoryCache.TryGetValue(cacheKey, out bookings))
-            {
-                bookings = new List<Booking>();
-            }
-
             bookings.Add(booking);
-            _memoryCache.Set(cacheKey, bookings);
             return View("Index", bookings);
         }
     }
--- Abgaben/Einzelabgaben/Witoschek/Uebungsblatt 4/Models/Booking.cs
+++ Abgaben/Einzelabgaben/Witoschek/Uebungsblatt 4/Models/Booking.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,17 +7,9 @@ namespace Blatt03.Models
 {
     public class Booking
     {
-        [Range(0, 100)]
-        [Required]
         public double currentCharge { get; set; }
-        [Range(0, 1000)]
-        [Required]
         public int requiredDistance { get; set; }
-        [DataType(DataType.DateTime)]
-        [Required]
         public DateTime start { get; set; }
-        [DataType(DataType.DateTime)]
-        [Required]
         public DateTime end { get; set; }
       
     }
