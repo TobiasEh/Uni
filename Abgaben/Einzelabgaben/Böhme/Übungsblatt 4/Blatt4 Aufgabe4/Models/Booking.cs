effac8e3e5c9ae2b25a06af4e2942e2518b69c71
boehmeth <thomas.boehme@student.uni-augsburg.de>
2020-05-18 17:49:06 +0200

    b4ü4

---
+++ Abgaben/Einzelabgaben/Böhme/Übungsblatt 4/Blatt4 Aufgabe4/Models/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blatt3_Aufgabe4.Models
+{
+	public class Booking
+	{
+		[Range(0, 100)]
+		[Required]
+		public int chargeStatus { get; set; }
+		[Range(1, 1000)]
+		[Required]
+		public int distance { get; set; }
+		[DataType(DataType.DateTime)]
+		[Required]
+		public DateTime startTime { get; set; }
+		[DataType(DataType.DateTime)]
+		[Required]
+		public DateTime endTime { get; set; }
+
+		public Booking()
+		{
+
+		}
+	}
+}
