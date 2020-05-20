4e807d67f7e5ab55032b1c90840dd3b1fceac55f
Tobias Eh <tobieh99@gmail.com>
2020-05-20 17:36:42 +0200

    Verbesserung

--- Abgaben/Einzelabgaben/Eh/Übungsblatt3/Aufgabe 4/Models/Booking.cs
+++ Abgaben/Einzelabgaben/Eh/Übungsblatt3/Aufgabe 4/Models/Booking.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Aufgabe_4.Models
 {
     public class Booking
     {
+        [Range(0, 100)]
+        [Required]
         public double currentCharge { get; set; }
+
+        [Range(0, 1000)]
+        [Required]
         public int requiredDistance { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
         public DateTime start { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
         public DateTime end { get; set; }
     }
 }
