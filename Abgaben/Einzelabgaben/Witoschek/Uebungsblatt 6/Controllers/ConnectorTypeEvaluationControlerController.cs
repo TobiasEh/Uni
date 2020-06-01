c781a5ccb985b9868c31e657f5c6386ea8ca5eb9
PimpMochi <62108424+PimpMochi@users.noreply.github.com>
2020-06-01 16:50:36 +0200

    Komponentendiagramm

---
+++ Abgaben/Einzelabgaben/Witoschek/Uebungsblatt 6/Controllers/ConnectorTypeEvaluationControlerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blatt03.Controllers
+{
+    public class ConnectorTypeEvaluationControlerController : Controller
+    {
+        public IActionResult Overview()
+        {
+            return View();
+        }
+    }
+}
---
+++ Abgaben/Einzelabgaben/Witoschek/Uebungsblatt 6/ViewModel/ConnectorTypeEvaluationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Blatt03.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blatt03.ViewModel
+{
+    public class ConnectorTypeEvaluationViewModel
+    {
+        public ConnectorType connectorType { get; set; }
+        public double percAppearsInBookings { get; set; }
+
+    }
+}
