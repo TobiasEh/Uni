e8ebaa85324ef8c893c2018f0a102f8c40ef7348
boehmeth <thomas.boehme@student.uni-augsburg.de>
2020-06-04 12:24:52 +0200

    B6A4-5-6 - TB

---
+++ Abgaben/Einzelabgaben/Böhme/Übungsblatt 6/Blatt6 Aufgabe4/DataValidation/DataValidationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blatt3_Aufgabe4.DataValidation
+{
+    public class DataValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime = Convert.ToDateTime(value);
+            return dateTime >= DateTime.Now;
+        }
+
+
+
+    }
+}
