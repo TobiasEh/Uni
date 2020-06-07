c2a71376595bfabecccdeb85896c054d4a93d41e
Tobias Eh <tobieh99@gmail.com>
2020-06-07 17:33:56 +0200

    Help
    erste version der help seite ohne erklärungen

--- Abgaben/Gruppe/Übungsblatt 5/Storyboard/Storyboard/Controllers/HomeController.cs
+++ Abgaben/Gruppe/Übungsblatt 5/Storyboard/Storyboard/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Storyboard.Controllers
             return View();
         }
 
+        public IActionResult Help()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
