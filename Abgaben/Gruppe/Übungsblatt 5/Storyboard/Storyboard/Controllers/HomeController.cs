f64deb9f867f5bf1f8ff990d32980d7fc495d0e7
PimpMochi <62108424+PimpMochi@users.noreply.github.com>
2020-06-08 00:21:30 +0200

    Index

--- Abgaben/Gruppe/Übungsblatt 5/Storyboard/Storyboard/Controllers/HomeController.cs
+++ Abgaben/Gruppe/Übungsblatt 5/Storyboard/Storyboard/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace Storyboard.Controllers
             return View();
         }
 
+        public IActionResult Imprint()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
