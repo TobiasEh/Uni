b256b5f1f2957d68c1124cbb0680cf4cc763dc01
Ulrich Reiter <ulrich.reiter@student.uni-augsburg.de>
2020-06-10 16:08:18 +0200

    Reset

--- Abgaben/Einzelabgaben/Reiter/Übungsblatt 6/NUnitTestProject/UnitTest1.cs
+++ Abgaben/Einzelabgaben/Reiter/Übungsblatt 6/NUnitTestProject/UnitTest1.cs
@@ -14,16 +14,7 @@ namespace NUnitTestProject
         [Test]
         public void TestAddBooking()
         {
-            var range = new System.ComponentModel.DataAnnotations.RangeAttribute(0, 100);
-            var dateValidation = new DateValidation();
-
-            Booking b1 = new Booking { currentCharge = 12, requiredDistance = 100, connectorType = ConnectorType.Type2Plug, start = new DateTime(2020, 12, 12, 12, 0, 0), end = new DateTime(2020, 12, 12, 14, 0, 0) };
-            foreach(Booking b in bookings)
-            {
-                Assert.IsTrue(range.IsValid(b.currentCharge));
-                Assert.IsTrue(dateValidation.IsValid(b.start));
-                Assert.IsTrue(dateValidation.IsValid(b.end));
-            }
+            
 
 
         }
