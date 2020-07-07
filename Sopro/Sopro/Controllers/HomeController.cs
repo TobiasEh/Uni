d7cc870610dce951dcf63e1f0f23cf90886c2aa6
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-07-07 21:24:42 +0200

    #87

--- Sopro/Sopro/Controllers/HomeController.cs
+++ Sopro/Sopro/Controllers/HomeController.cs
@@ -1,37 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Sopro.Models;
+using Sopro.Models.User;
 
 namespace Sopro.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Help()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Imprint()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }      
+        public IActionResult Login(string email)
+        {
+            if(IdentityProvider.getUserPriority(email) != UserType.PLANER)
+            {
+                return RedirectToAction("Index", "Booking");
+            }
+            return View();
         }
     }
 }
