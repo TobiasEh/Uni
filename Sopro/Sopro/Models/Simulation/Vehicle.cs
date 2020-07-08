795e7c2613928bb513e2479d78390312233d11ab
Ta1rasha <45498054+Ta1rasha@users.noreply.github.com>
2020-07-09 00:09:39 +0200

    added vehicle class

---
+++ Sopro/Sopro/Models/Simulation/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using Foolproof;
+using Sopro.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Sopro.Models.Infrastructure;
+
+namespace Sopro.Models.Simulation
+{
+    public class Vehicle
+    {
+        [Required]
+        public string model { get; set; }
+        [Required]
+        [BookingCapacityValidation]
+        public int capacity { get; set; }
+        [Required]
+        [BookingPlugsValidation]
+        public List<PlugType> plugs { get; set; }
+
+        //public PlugType[] plugs { get; set; }
+        [Required]
+        [BookingSocStartValidation]
+        public int socStart { get; set; }
+        [Required]
+        [BookingSocEndValidation]
+        [GreaterThan("socStart")]
+        public int socEnd { get; set; }
+    }
+}
