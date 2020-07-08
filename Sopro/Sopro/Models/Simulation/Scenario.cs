45462098f32e0a9c68fdac2118f4fb9d6e0c2510
Ta1rasha <45498054+Ta1rasha@users.noreply.github.com>
2020-07-09 00:42:07 +0200

    Added Scenario attributes

--- Sopro/Sopro/Models/Simulation/Scenario.cs
+++ Sopro/Sopro/Models/Simulation/Scenario.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Sopro.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,18 @@ namespace Sopro.Models.Simulation
 {
     public class Scenario
     {
+        [Required]
+        public string id { get; set; }
+        [Required]
+        [BookingCapacityValidation]
+        public int duration { get; set; }
+        [Required]
+        [BookingCapacityValidation]
+        public int bookingCount { get; set; }
+        [Required]
+        public List<Vehicle> vehicles { get; set; }
+        [Required]
+        public List<Rushhour> rushours { get; set; }
+        
     }
 }
