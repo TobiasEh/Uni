8bd0cb3b4d2df281744eb0419987e04a26bdd73b
boehmeth <thomas.boehme@student.uni-augsburg.de>
2020-07-09 20:16:50 +0200

    Create NotificatioinManager.cs

---
+++ Sopro/Sopro/Models/Administration/NotificatioinManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sopro.Models.Administration
+{
+    public class NotificatioinManager
+    {
+        private INotificationListener listener { get; set; }
+
+        public notifiy(Booking booking, String eventName)
+        {
+                
+        }
+    }
+}
