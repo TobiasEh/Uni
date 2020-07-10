a69dd358be2f9b9847adc733657439f41d975cf0
boehmeth <thomas.boehme@student.uni-augsburg.de>
2020-07-10 14:34:16 +0200

    finish messenger

--- Sopro/Sopro/Models/Communication/Messenger.cs
+++ Sopro/Sopro/Models/Communication/Messenger.cs
@@ -14,14 +14,19 @@ namespace Sopro.Models.Communication
         {
             emailClient = new SmtpClient();
         }
+        /* Method for sending an Email to spezific user
+         */
         public void sendMessage(String message, String user)
         {
             var finalMessage = new MimeMessage();
-            ;
+            finalMessage.From.Add(new MailboxAddress("Ladesäulensystem MHP", "SoProSender@outlook.de"));
             finalMessage.To.Add(MailboxAddress.Parse(user));
             finalMessage.Subject = "Ladesäulenbuchung MHP";
-            finalMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = message };
-            emailClient.Connect()
+            finalMessage.Body = new TextPart(MimeKit.Text.TextFormat.Plain) { Text = message };
+            emailClient.Connect("SMTP.office365.com", 587, true);
+            emailClient.Authenticate("soprosender@outlook.de", "SoPro2020Sender");
+            emailClient.Send(finalMessage);
+            emailClient.Disconnect(true);
 
         }
     }
