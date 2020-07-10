82f6b4e59d53585b39ac1f4dc11a8cd0fab3c1a7
ga27man <ga27man@mytum.de>
2020-07-10 16:00:29 +0200

    copied vehicle.cs from administration to simulation

---
+++ Sopro/Sopro/Models/Simulation/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using Sopro.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Sopro.Models.Infrastructure;
+using sopro2020_abgabe.Models;
+
+namespace Sopro.Models.Simulation
+{
+    public class Vehicle
+    {
+        [Required]
+        public string model { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int capacity { get; set; }
+        public Plug plugs { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int socStart { get; set; }
+        [Required]
+        [VehicleSocEndValidation]
+        public int socEnd { get; set; }
+    }
+}
