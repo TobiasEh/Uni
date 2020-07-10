96bd1013d84d445d253370aec4deeaeb6a28b060
Ulrich Reiter <ulrich.reiter@student.uni-augsburg.de>
2020-07-10 16:38:38 +0200

    IDistributionStrategy + StandardDsitribution

---
+++ Sopro/Sopro/Interfaces/IDistributionStrategy.cs
@@ -0,0 +1,14 @@
+﻿using Sopro.Models.Administration;
+using Sopro.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sopro.Interfaces
+{
+    interface IDistributionStrategy
+    {
+        bool distribute(List<Booking> bookings, Schedule schedule, int puffer);
+    }
+}
