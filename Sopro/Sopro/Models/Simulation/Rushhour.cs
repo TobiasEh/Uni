3505d8c5d6b94b311bf65e4d051a4fe509f83ba6
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-07-10 21:36:41 +0200

    #119

---
+++ Sopro/Sopro/Models/Simulation/Rushhour.cs
@@ -0,0 +1,22 @@
+﻿using Sopro.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sopro.Models.Simulation
+{
+    public class Rushhour
+    {
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public int bookings { get; set; }
+        public IFunctionStrategy strategy { get; set; }
+
+        public List<DateTime> run()
+        {
+
+            return strategy.generateDateTimeValues(start,end,bookings);
+        }
+    }
+}
