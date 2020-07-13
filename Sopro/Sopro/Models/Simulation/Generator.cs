5323690653a68cb911aa55b03fa70d254c445115
PimpMochi <62108424+PimpMochi@users.noreply.github.com>
2020-07-14 01:27:05 +0200

    Update Generator.cs

--- Sopro/Sopro/Models/Simulation/Generator.cs
+++ Sopro/Sopro/Models/Simulation/Generator.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.Extensions.FileSystemGlobbing.Internal;
-using Sopro.Models.Administration;
+﻿using Sopro.Models.Administration;
 using Sopro.Models.Infrastructure;
+using Sopro.Models.User;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using Sopro.Interfaces.AdministrationSimulation;
 
 namespace Sopro.Models.Simulation
 {
     public class Generator : IGenerator
     {
+        [Range(0,1)]
+        public double probabilityVIP { get; set; }
+        [Range(0, 1)]
+        public double probabilityGUEST { get; set; }
+
+        public Generator()
+        {
+            probabilityGUEST = 0.05;
+            probabilityVIP = 0.05;
+        }
         public List<Booking> generateBookings(Scenario scenario) 
         {
             List<Booking> bookingList = new List<Booking>();
@@ -40,10 +52,10 @@ namespace Sopro.Models.Simulation
                                     endTime = start.AddHours(new Random().Next(1, 8)),
                                     station = null,
                                     active = false,
-                                    priority = User.UserType.ASSISTANCE,
+                                    priority = setPrio(),
                                     location = scenario.location
                                 }
-                                );
+                                ); ;
                         }
                         
                     }
@@ -54,15 +66,33 @@ namespace Sopro.Models.Simulation
                 else
                 {
                     LinearDist(bookingList,scenario,i);
-                    
-
-                    // fill with bookings linear
                 }
             }
             
 
             return new List<Booking>();
         }
+
+        private UserType setPrio()
+        {
+            double prob = new Random().NextDouble();
+            if (prob > probabilityVIP + probabilityGUEST)
+            {
+                return UserType.EMPLOYEE;
+            }
+            else
+            {
+                if (prob > probabilityGUEST)
+                {
+                    return UserType.VIP;
+                }
+                else
+                {
+                    return UserType.GUEST;
+                }
+            }
+        }
+
         public void LinearDist(List<Booking> bookingList, Scenario scenario, int i)
         {
             int r = new Random().Next(scenario.vehicles.Count);
