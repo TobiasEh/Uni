64be763ec93bcae97f736db5fe46721920f4478f
PimpMochi <62108424+PimpMochi@users.noreply.github.com>
2020-07-16 23:47:24 +0200

    Update Schedule.cs

--- Sopro/Sopro/Models/Administration/Schedule.cs
+++ Sopro/Sopro/Models/Administration/Schedule.cs
@@ -37,7 +37,7 @@ namespace Sopro.Models.Administration
             {
                 return false;
             }
-            notificationManager.notify(booking, NotificationEvent.ACCEPTED);
+            // notificationManager.notify(booking, NotificationEvent.ACCEPTED);
 
             return true;
         }
