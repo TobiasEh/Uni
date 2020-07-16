a9430497a92cb293f63edf9ca0e750d72258766b
PimpMochi <62108424+PimpMochi@users.noreply.github.com>
2020-07-16 23:10:15 +0200

    Fixes

--- Sopro/Sopro/Models/History/CondenseInfrastructureSuggestion.cs
+++ Sopro/Sopro/Models/History/CondenseInfrastructureSuggestion.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Sopro.Models.History
+﻿namespace Sopro.Models.History
 {
     public class CondenseInfrastructureSuggestion : Suggestion
     {
