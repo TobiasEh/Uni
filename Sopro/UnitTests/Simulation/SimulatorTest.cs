a3630228698dc8fac858ad7924906e9f40d4b3a3
PimpMochi <62108424+PimpMochi@users.noreply.github.com>
2020-07-17 22:56:56 +0200

    idk

--- Sopro/UnitTests/Simulation/SimulatorTest.cs
+++ Sopro/UnitTests/Simulation/SimulatorTest.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿using NUnit.Framework;
+using Sopro.Interfaces;
+using Sopro.Models.Infrastructure;
+using Sopro.Models.Simulation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace UnitTests.Simulation
+namespace UnitTests.SimulationTest
 {
+    [TestFixture]
     class SimulatorTest
     {
+        private ILocation location = new Location()
+        {
+            name = "Berlin",
+            emergency = 30.2,
+        };
+
+        private Vehicle vehicle = new Vehicle()
+        {
+            capacity = 120,
+            model = "Porsche",
+            socStart = 24,
+            socEnd = 50,
+            plugs = new List<PlugType>()
+            {
+                PlugType.CCS
+            }
+        };
+
+        private Vehicle vehicle2 = new Vehicle()
+        {
+            capacity = 140,
+            model = "Porsche",
+            socStart = 60,
+            socEnd = 100,
+            plugs = new List<PlugType>()
+            {
+                PlugType.CCS
+            }
+        };
+
+        [Test]
+        public void testSimulator()
+        {
+            Simulator sim = new Simulator()
+            {
+
+            };
+        }
     }
 }
