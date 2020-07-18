e23230895ce6d2eebd07f0921db308126d9afa2a
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-07-18 21:21:19 +0200

    wip #178

---
+++ Sopro/Sopro/ViewModels/EditCarTemplateVieModel.cs
@@ -0,0 +1,20 @@
+﻿using Sopro.Interfaces.ControllerSimulation;
+using Sopro.Models.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sopro.ViewModels
+{
+    public class EditCarTemplateVieModel : IVehicle
+    {
+        public string model { get; set; }
+        public int capacity { get; set; }
+        public int socStart { get; set; }
+        public int socEnd { get; set; }
+        public List<PlugType> plugs { get; set; }
+        public int index { get; set; }
+    }
+}
