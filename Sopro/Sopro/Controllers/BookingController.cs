b3a99c6ff3d1a4d6fa51bedb07f55392a6162920
Ta1rasha <45498054+Ta1rasha@users.noreply.github.com>
2020-07-19 19:08:04 +0200

    booking edit fertig

--- Sopro/Sopro/Controllers/BookingController.cs
+++ Sopro/Sopro/Controllers/BookingController.cs
@@ -102,11 +102,6 @@ namespace Sopro.Controllers
                 booking.startTime = now;
                 booking.endTime = now;
             }
-            var email = HttpContext.Session.GetString("email");
-            if (booking.user == null)
-            {
-                booking.user = email;
-            }
             return View("Create", new BookingCreateViewModel(locations, booking, booking.plugs.Contains(PlugType.CCS), booking.plugs.Contains(PlugType.TYPE2)));           
         }
 
@@ -187,13 +182,24 @@ namespace Sopro.Controllers
          * Booking is removed from bookinglist and cache.
          * Returns Booking.Create view with already filled fields.
          */
-        public IActionResult Edit(Booking booking)
+        public IActionResult Edit(string bookingID)
         {
             var cacheKey = CacheKeys.BOOKING;
             if (!cache.TryGetValue(cacheKey, out bookings))
             {
                 bookings = new List<IBooking>();
             }
+            Booking booking = null;
+            foreach (Booking b in bookings)
+            {
+                if (b.id.Equals(bookingID))
+                {
+                    booking = b;
+                    break;
+                }
+
+            }
+            
             bookings.Remove(booking);
             cache.Set(cacheKey, bookings);
             List<ILocation> locations;
