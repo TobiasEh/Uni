d12134033687c7194e0a47d9d1d3c9cbc52df976
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-07-20 03:56:32 +0200

    update wip

--- Sopro/Sopro/ViewModels/ScenarioCreateViewModel.cs
+++ Sopro/Sopro/ViewModels/ScenarioCreateViewModel.cs
@@ -2,6 +2,7 @@
 using Sopro.Interfaces;
 using Sopro.Interfaces.ControllerSimulation;
 using Sopro.Models.Simulation;
+using Sopro.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,8 +19,18 @@ namespace Sopro.ViewModels
         public ILocation location { get; set; }
         public int tickLength { get; set; }
         public List<int> vcount{get;set;}
+        public ScenarioLocationViewModel sclocation { get; set; }
+        public ScenarioCreateZoneViewModel sczone { get; set; }
+
+
         public ScenarioCreateViewModel(List<ILocation> _locations, List<Vehicle> _vehicles)
         {
+            sclocation = new Sopro.ViewModels.ScenarioLocationViewModel();
+            sclocation.location = new Sopro.Models.Infrastructure.Location();
+            sclocation.location.name = "Neu";
+            sclocation.location.emergency = 0;
+            sclocation.location.zones = new List<Models.Infrastructure.Zone>();
+
             locations = _locations;
             vehicles = _vehicles;
             vcount = new List<int>(vehicles.Count);
@@ -29,6 +40,12 @@ namespace Sopro.ViewModels
 
         public ScenarioCreateViewModel() 
         {
+            sclocation = new Sopro.ViewModels.ScenarioLocationViewModel();
+            sclocation.location = new Sopro.Models.Infrastructure.Location();
+            sclocation.location.name = "Neu";
+            sclocation.location.emergency = 0;
+            sclocation.location.zones = new List<Models.Infrastructure.Zone>();
+
             locations = new List<ILocation>();
             vehicles = new List<Vehicle>();
             rushhours = new List<Rushhour>();
