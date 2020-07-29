3b011dd0533ee3abd09725608120112bb6e0187d
Thomas Böhme <thomas.boehme@student.uni-augsburg.de>
2020-07-29 13:09:17 +0200

    GeneratorTest aukommentiert für Pipeline

--- Code/UnitTests/Simulation/GeneratorTest.cs
+++ Code/UnitTests/Simulation/GeneratorTest.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+﻿/*using NUnit.Framework;
 using NUnit.Framework.Internal;
 using Sopro.Models.Administration;
 using Sopro.Models.Infrastructure;
@@ -394,3 +394,4 @@ namespace UnitTests.Simulation
         }
     }
 }
+*/
