2e15da13d5797ba62b16d49933da5f7ce8df3f33
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-08-01 01:17:55 +0200

    animation login

---
+++ Code/SoPro/wwwroot/js/login.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#navlinkB').click(function () {
+        $('#loginBox').addClass('animate__animated animate__shakeX rounded border border-danger');
+        $('.alert').fadeIn(500);
+        setTimeout("$('.alert').fadeOut(1500);", 3000);
+    });
+    $('.form-control').keypress(function () {
+        $('#loginBox').removeClass('animate__animated animate__shakeX rounded border border-danger');
+    });
+
+});
