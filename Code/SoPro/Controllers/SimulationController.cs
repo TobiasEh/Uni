75abab22aafc040c12762df0750232b3eaf05497
PimpMochi <62108424+PimpMochi@users.noreply.github.com>
2020-08-01 18:06:25 +0200

    Update and visual improvement to pie chart

--- Code/SoPro/Controllers/SimulationController.cs
+++ Code/SoPro/Controllers/SimulationController.cs
@@ -11,6 +11,7 @@ using Sopro.ViewModels;
 using Sopro.Interfaces.PersistenceController;
 using Sopro.Models.Infrastructure;
 using Sopro.Persistence.PersScenario;
+using Sopro.ViewModels.TestViewModels;
 
 namespace Sopro.Controllers
 {
@@ -599,7 +600,22 @@ namespace Sopro.Controllers
 
         public IActionResult Evaluation()
         {
-            return View();
+            Random rnd = new Random();
+
+            var lstModel = new List<DataViewModel>();
+            lstModel.Add(new DataViewModel
+            {
+                DimensionOne = "Type-2",
+                Quantity = rnd.Next(10)
+            });
+            lstModel.Add(new DataViewModel
+            {
+                DimensionOne = "CCS",
+                Quantity = rnd.Next(10)
+            });
+
+            return View("Evaluation", lstModel);
+            // return View();
         }
     }
 }
