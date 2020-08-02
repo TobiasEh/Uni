c90f399bfd78fa65af976f47c1736d6918975cb4
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-08-02 18:07:50 +0200

    Update CustomValidation.js

--- Code/SoPro/wwwroot/js/CustomValidation.js
+++ Code/SoPro/wwwroot/js/CustomValidation.js
@@ -1,18 +1,19 @@
 ﻿$.validator.addMethod('BookingStartTimeValidation', function (value, element) {
         return Date.parse(value) <= Date.now;
 });
-$.validator.addMethod("BookingEndTimeValidation", function (value, element, params) {
-    return Date.parse(value) > Date.parse($(params).val());
+$.validator.addMethod("BookingEndTimeValidation", function (value, element) {
+    return Date.parse(value) > Date.parse($('#booking_startTime').val());
 });
+
+$.validator.addMethod('BookingSocEndValidation', function (value, element) {
+    return parseInt(value) > parseInt($('#booking_socStart').val());
+});
+
+
+
 
 
 
 $.validator.unobtrusive.adapters.addBool('BookingStartTimeValidation');
-$.validator.unobtrusive.adapters.add('classicmovie', ['year'], function (options) {
-    var element = $(options.form).find('select#Movie_Genre')[0];
-
-    options.rules['BookingStartTimeValidation'] = [element, parseInt(options.params['year'])];
-    options.messages['BookingStartTimeValidation'] = options.message;
-});
-// Value is the element to be validated, params is the array of name/value pairs of the parameters extracted from the HTML, element is the HTML element that the validator is attached to
-booking_startTime
+$.validator.unobtrusive.adapters.addBool('BookingEndTimeValidation');
+$.validator.unobtrusive.adapters.addBool('BookingSocEndValidation');
