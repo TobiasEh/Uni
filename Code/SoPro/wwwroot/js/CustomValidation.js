04bc7c10a6f5b796cb47c9a34efd24f769d45962
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-08-02 23:43:32 +0200

    update

--- Code/SoPro/wwwroot/js/CustomValidation.js
+++ Code/SoPro/wwwroot/js/CustomValidation.js
@@ -1,43 +1,44 @@
-﻿$.validator.addMethod("starttime", function (value, element) {
-    return Date.parse(value) > Date.now();
-});
-$.validator.addMethod("endtime", function (value, element) {
-    return Date.parse(value) > Date.parse($('#booking_startTime').val());
-});
+﻿$(document).ready(function () {
+    $.validator.addMethod("starttime", function (value, element) {
+        return false;
+    });
+    $.validator.addMethod("endtime", function (value, element) {
+        return Date.parse(value) > Date.parse($('#booking_startTime').val());
+    });
 
-$.validator.addMethod('socend', function (value, element,  params) {
-    return parseInt(value) > parseInt($('#booking_socStart').val());
-});
-$.validator.addMethod('minPlugtype2Validation', function (value, element) {
-    if ($('#ccs').val() == 0 && parseInt(value) == 0) {
-        return false;
-    }
-    return true;
-});
-$.validator.addMethod('minPlugccsValidation', function (value, element) {
-    if ($('#type2').val() == 0 && parseInt(value) == 0) {
-        return false;
-    }
-    return true;
-});
-$.validator.addMethod('ccsPowerValidation', function (value, element) {
-    if ($('#ccs').val() != 0 && parseInt(value) == 0 ) {
-        return false;
-    }
-    return true;
-});
-$.validator.addMethod('type2PowerValidation', function (value, element) {
-    if ($('#type2').val() != 0 && parseInt(value) == 0) {
-        return false;
-    }
-    return true;
-});
-$.validator.addMethod('parallelUsableValidation', function (value, element) {
-    return (parseInt($('#type2').val()) + parseInt($('#ccs').val())) > parseInt(value);
-});
+    $.validator.addMethod('socend', function (value, element, params) {
+        return parseInt(value) > parseInt($('#booking_socStart').val());
+    });
+    $.validator.addMethod('minPlugtype2Validation', function (value, element) {
+        if ($('#ccs').val() == 0 && parseInt(value) == 0) {
+            return false;
+        }
+        return true;
+    });
+    $.validator.addMethod('minPlugccsValidation', function (value, element) {
+        if ($('#type2').val() == 0 && parseInt(value) == 0) {
+            return false;
+        }
+        return true;
+    });
+    $.validator.addMethod('ccsPowerValidation', function (value, element) {
+        if ($('#ccs').val() != 0 && parseInt(value) == 0) {
+            return false;
+        }
+        return true;
+    });
+    $.validator.addMethod('type2PowerValidation', function (value, element) {
+        if ($('#type2').val() != 0 && parseInt(value) == 0) {
+            return false;
+        }
+        return true;
+    });
+    $.validator.addMethod('parallelUsableValidation', function (value, element) {
+        return (parseInt($('#type2').val()) + parseInt($('#ccs').val())) > parseInt(value);
+    });
 
-$.validator.unobtrusive.adapters.add('starttime', ['year'], function (options) {
-    options.messages['classicmovie'] = options.message;
+    $.validator.unobtrusive.adapters.add('starttime', ['year'], function (options) {
+        options.messages['classicmovie'] = options.message;
+    });
+    $.validator.unobtrusive.adapters.addBool(endtime);
 });
-$.validator.unobtrusive.adapters.addBool(endtime);
-
