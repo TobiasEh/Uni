48d43e15eddf4267caafd1f88c4b767b6dd587f3
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-08-02 18:54:38 +0200

    Update CustomValidation.js

--- Code/SoPro/wwwroot/js/CustomValidation.js
+++ Code/SoPro/wwwroot/js/CustomValidation.js
@@ -8,7 +8,33 @@ $.validator.addMethod("BookingEndTimeValidation", function (value, element) {
 $.validator.addMethod('BookingSocEndValidation', function (value, element) {
     return parseInt(value) > parseInt($('#booking_socStart').val());
 });
-
+$.validator.addMethod('minPlugtype2Validation', function (value, element) {
+    if ($('#ccs').val() == 0 && parseInt(value) == 0) {
+        return false;
+    }
+    return true;
+});
+$.validator.addMethod('minPlugccsValidation', function (value, element) {
+    if ($('#type2').val() == 0 && parseInt(value) == 0) {
+        return false;
+    }
+    return true;
+});
+$.validator.addMethod('ccsPowerValidation', function (value, element) {
+    if ($('#ccs').val() != 0 && parseInt(value) == 0 ) {
+        return false;
+    }
+    return true;
+});
+$.validator.addMethod('type2PowerValidation', function (value, element) {
+    if ($('#type2').val() != 0 && parseInt(value) == 0) {
+        return false;
+    }
+    return true;
+});
+$.validator.addMethod('parallelUsableValidation', function (value, element) {
+    return (parseInt($('#type2').val()) + parseInt($('#ccs').val())) > parseInt(value);
+});
 
 
 
