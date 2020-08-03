1a64a691235a8013488c4c75013dbc888650a2fd
Nguyen Thao Dat <thao.nguyen@student.uni-augsburg.de>
2020-08-03 11:31:16 +0200

    Update CustomValidation.js

--- Code/SoPro/wwwroot/js/CustomValidation.js
+++ Code/SoPro/wwwroot/js/CustomValidation.js
@@ -1,6 +1,4 @@
-﻿$.validator.setDefaults({
-    debug: true
-});
+﻿
 $.validator.addMethod("starttime", function (value, element) {
     return Date.parse(value) > Date.now();
 });
@@ -16,34 +14,11 @@ $.validator.addMethod('socend', function (value, element) {
 });
 
 
-$.validator.addMethod('minplug', function (value, element) {
-    if ($('input:checkbox:checked').length > 0) {
-        return true;
-    }
-    return false;
-});
-
-$.validator.addMethod('atleastoneplug', function (value, element) {
-    if ($('#ccs').val() + $('#type2').val() > 0) {
-        return true;
-    }
-    return false;
-});
-
-$.validator.addMethod('notzeropower', function (value, element) {
-    var id = '#' + element.attr('id').replace('Power', '');
-    if ($(id).val() > 0 && value == 0) {
-        return false;
-    }
-    return true;
-});
 
 $.validator.unobtrusive.adapters.addBool('starttime');
 $.validator.unobtrusive.adapters.addBool('endtime');
 $.validator.unobtrusive.adapters.addBool('socend');
-$.validator.unobtrusive.adapters.addBool('minplug');
-$.validator.unobtrusive.adapters.addBool('atleastoneplug');
-$.validator.unobtrusive.adapters.addBool('notzeropower');
+
 $.validator.unobtrusive.parse('form');
 
 
